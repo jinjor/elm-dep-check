55fcedf16013be9e26759fb427d4d7253c224c30
jinjor <jinjorweb@gmail.com>
2016-08-06 17:18:41 +0900

    introduce argv

--- cli.js
+++ cli.js
@@ -1,1 +1,13 @@
-require('./index.js').report();
+var argv = require('argv');
+
+argv.option({
+	name: 'output',
+	short: 'o',
+	type : 'string',
+	description : 'output file, default is `./dep-check.html`',
+	example: "'elm-dep-check --output=report.html' or 'elm-dep-check -o report.html"
+});
+
+var args = argv.run();
+
+require('./index.js').report(args.options);
